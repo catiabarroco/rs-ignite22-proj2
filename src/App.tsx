6ad84114784f2bfbd8e7a750eed360327bfccc57
Catia Barroco <104831678+CatiaBarroco-xgeeks@users.noreply.github.com>
2023-04-26 15:50:57 +0100

    router context

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,15 @@ import {ThemeProvider} from "styled-components"
 import {GlobalStyle} from './styles/global'
 import {BrowserRouter} from "react-router-dom";
 import {Router} from "./Router";
+import { CyclesContextProvider } from './contexts/CyclesContext'
 
 export function App() {
-
     return (
         <ThemeProvider theme={defaultTheme}>
             <BrowserRouter>
-                <Router/>
+                <CyclesContextProvider>
+                    <Router />
+                </CyclesContextProvider>
             </BrowserRouter>
             <GlobalStyle/>
         </ThemeProvider>
