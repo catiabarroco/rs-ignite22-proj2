1def683d67dda2f38eb46efcb988a5be2f143491
Catia Barroco <104831678+CatiaBarroco-xgeeks@users.noreply.github.com>
2023-05-02 09:54:42 +0100

    fix build

--- src/contexts/CyclesContext.tsx
+++ src/contexts/CyclesContext.tsx
@@ -1,109 +1,108 @@
-import { differenceInSeconds } from 'date-fns'
+import { differenceInSeconds } from 'date-fns';
 import {
-    createContext,
-    ReactNode,
-    useEffect,
-    useReducer,
-    useState,
-} from 'react'
+  createContext,
+  ReactNode,
+  useEffect,
+  useReducer,
+  useState,
+} from 'react';
 import {
-    ActionTypes,
-    addNewCycleAction,
-    interruptCurrentCycleAction,
-    markCurrentCycleAsFinishedAction,
-} from '../reducers/cycles/actions'
-import { Cycle, cyclesReducer } from '../reducers/cycles/reducer'
+  addNewCycleAction,
+  interruptCurrentCycleAction,
+  markCurrentCycleAsFinishedAction,
+} from '../reducers/cycles/actions';
+import { Cycle, cyclesReducer } from '../reducers/cycles/reducer';
 interface CreateCycleData {
-    task: string
-    minutesAmount: number
+  task: string;
+  minutesAmount: number;
 }
 interface CyclesContextType {
-    cycles: Cycle[]
-    activeCycle: Cycle | undefined
-    activeCycleId: string | null
-    amountSecondsPassed: number
-    markCurrentCycleAsFinished: () => void
-    setSecondsPassed: (seconds: number) => void
-    createNewCycle: (data: CreateCycleData) => void
-    interruptCurrentCycle: () => void
+  cycles: Cycle[];
+  activeCycle: Cycle | undefined;
+  activeCycleId: string | null;
+  amountSecondsPassed: number;
+  markCurrentCycleAsFinished: () => void;
+  setSecondsPassed: (seconds: number) => void;
+  createNewCycle: (data: CreateCycleData) => void;
+  interruptCurrentCycle: () => void;
 }
-export const CyclesContext = createContext({} as CyclesContextType)
+export const CyclesContext = createContext({} as CyclesContextType);
 interface CyclesContextProviderProps {
-    children: ReactNode
+  children: ReactNode;
 }
 export function CyclesContextProvider({
-                                          children,
-                                      }: CyclesContextProviderProps) {
-    const [cyclesState, dispatch] = useReducer(
-        cyclesReducer,
-        {
-            cycles: [],
-            activeCycleId: null,
-        },
-        (initialState) => {
-            const storedStateAsJSON = localStorage.getItem(
-                '@ignite-timer:cycles-state-1.0.0',
-            )
+  children,
+}: CyclesContextProviderProps) {
+  const [cyclesState, dispatch] = useReducer(
+    cyclesReducer,
+    {
+      cycles: [],
+      activeCycleId: null,
+    },
+    (initialState) => {
+      const storedStateAsJSON = localStorage.getItem(
+        '@ignite-timer:cycles-state-1.0.0',
+      );
 
-            if (storedStateAsJSON) {
-                return JSON.parse(storedStateAsJSON)
-            }
+      if (storedStateAsJSON) {
+        return JSON.parse(storedStateAsJSON);
+      }
 
-            return initialState
-        },
-    )
+      return initialState;
+    },
+  );
 
-    const { cycles, activeCycleId } = cyclesState
-    const activeCycle = cycles.find((cycle) => cycle.id === activeCycleId)
+  const { cycles, activeCycleId } = cyclesState;
+  const activeCycle = cycles.find((cycle) => cycle.id === activeCycleId);
 
-    const [amountSecondsPassed, setAmountSecondsPassed] = useState(() => {
-        if (activeCycle) {
-            return differenceInSeconds(new Date(), new Date(activeCycle.startDate))
-        }
-
-        return 0
-    })
-
-    useEffect(() => {
-        const stateJSON = JSON.stringify(cyclesState)
-
-        localStorage.setItem('@ignite-timer:cycles-state-1.0.0', stateJSON)
-    }, [cyclesState])
-
-    function setSecondsPassed(seconds: number) {
-        setAmountSecondsPassed(seconds)
+  const [amountSecondsPassed, setAmountSecondsPassed] = useState(() => {
+    if (activeCycle) {
+      return differenceInSeconds(new Date(), new Date(activeCycle.startDate));
     }
-    function markCurrentCycleAsFinished() {
-        dispatch(markCurrentCycleAsFinishedAction())
-    }
-    function createNewCycle(data: CreateCycleData) {
-        const id = String(new Date().getTime())
-        const newCycle: Cycle = {
-            id,
-            task: data.task,
-            minutesAmount: data.minutesAmount,
-            startDate: new Date(),
-        }
-        dispatch(addNewCycleAction(newCycle))
-        setAmountSecondsPassed(0)
-    }
-    function interruptCurrentCycle() {
-        dispatch(interruptCurrentCycleAction())
-    }
-    return (
-        <CyclesContext.Provider
-            value={{
-                cycles,
-                activeCycle,
-                activeCycleId,
-                markCurrentCycleAsFinished,
-                amountSecondsPassed,
-                setSecondsPassed,
-                createNewCycle,
-                interruptCurrentCycle,
-            }}
-        >
-            {children}
-        </CyclesContext.Provider>
-    )
+
+    return 0;
+  });
+
+  useEffect(() => {
+    const stateJSON = JSON.stringify(cyclesState);
+
+    localStorage.setItem('@ignite-timer:cycles-state-1.0.0', stateJSON);
+  }, [cyclesState]);
+
+  function setSecondsPassed(seconds: number) {
+    setAmountSecondsPassed(seconds);
+  }
+  function markCurrentCycleAsFinished() {
+    dispatch(markCurrentCycleAsFinishedAction());
+  }
+  function createNewCycle(data: CreateCycleData) {
+    const id = String(new Date().getTime());
+    const newCycle: Cycle = {
+      id,
+      task: data.task,
+      minutesAmount: data.minutesAmount,
+      startDate: new Date(),
+    };
+    dispatch(addNewCycleAction(newCycle));
+    setAmountSecondsPassed(0);
+  }
+  function interruptCurrentCycle() {
+    dispatch(interruptCurrentCycleAction());
+  }
+  return (
+    <CyclesContext.Provider
+      value={{
+        cycles,
+        activeCycle,
+        activeCycleId,
+        markCurrentCycleAsFinished,
+        amountSecondsPassed,
+        setSecondsPassed,
+        createNewCycle,
+        interruptCurrentCycle,
+      }}
+    >
+      {children}
+    </CyclesContext.Provider>
+  );
 }
